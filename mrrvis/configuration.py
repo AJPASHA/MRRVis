fixing rotation bugs

mrrvis/configuration.py
@@ -3,10 +3,8 @@
 from typing import Iterable
 import numpy as np
 import warnings
-from mrrvis.cell import Cell, Square,Hex, Tri, Cube
+from mrrvis.cell import Cell, Square, Hex, Tri, Cube
 from mrrvis.geometry_utils import rotate_normal, cube_rotation_list
-
-
 
 
 class ConfigurationGraph:
@@ -39,7 +37,7 @@ class ConfigurationGraph:
         if vertices is None:
             self.vertices = np.empty((0, self.Cell.n_parameters))
         else:
-            if len(vertices.shape) ==1:
+            if len(vertices.shape) == 1:
                 vertices = np.array([vertices])
             vertices = np.array(vertices).astype(int)
 
@@ -54,16 +52,14 @@ class ConfigurationGraph:
                 if not(self.Cell.valid_coord(vertex)):
                     print(
                         f"invalid cell detected at {vertex}, dropping from graph")
+                    vertices = np.delete(
+                        vertices, get_index(vertex, vertices), axis=0)
 
-                    #this doesn't make sense?
-                    
-                    vertices = np.delete(vertices, get_index(vertex, vertices), axis=0)
-            
             # We also need to drop duplicates!
             # np.unique doesn't preserve order, so we need to record the indices and perform argsort on the resulting array
-            vertices, ind = np.unique(vertices, return_index=True,axis=0)
-            vertices =  vertices[np.argsort(ind)]
-  
+            vertices, ind = np.unique(vertices, return_index=True, axis=0)
+            vertices = vertices[np.argsort(ind)]
+
             self.vertices = vertices
 
     def get_index(self, vertex):
@@ -80,7 +76,7 @@ class ConfigurationGraph:
         vertices = self.vertices
 
         # if there are fewer than 2 vertices, the graph is connected
-        if len(vertices)<=1:
+        if len(vertices) <= 1:
             return True
 
         for vertex in vertices:
@@ -146,45 +142,46 @@ class ConfigurationGraph:
         This is because the order of the rotation group of a cube is 24, which is still small enough to be brute forced,
         but in future a more efficient method might need to be considered
         """
-  
+
         self_verts = self.vertices
         other_verts = other.vertices
 
-        
-        #check cell types are the same 
+        # check cell types are the same
         if self.Cell != other.Cell:
             return False
-            
-        #1. center self graph on origin from most westerly, then southerly, then downward vertex
+
+        # 1. center self graph on origin from most westerly, then southerly, then downward vertex
         self_min = min_coord(self_verts)
         self_verts = self_verts - self_min
 
-        if self.Cell.dimensions==2:
+        if self.Cell.dimensions == 2:
 
             num_rots = (2*np.pi)/self.Cell.rotation_angle
-            if abs(num_rots-int(num_rots))>0.001:
-                raise ValueError("rotation angle must be a multiple of 2*pi/n_parameters")
+            if abs(num_rots-int(num_rots)) > 0.001:
+                raise ValueError(
+                    "rotation angle must be a multiple of 2*pi/n_parameters")
             num_rots = int(num_rots)
 
             for _ in range(num_rots):
 
-                #2. identify the 'smallest' vertex in other graph and translate to origin
+                # 2. identify the 'smallest' vertex in other graph and translate to origin
                 other_min = min_coord(other_verts)
                 other_verts_temp = other_verts - other_min
-                #3. verify that the graphs are isomorphic based on their vertex sets
+                # 3. verify that the graphs are isomorphic based on their vertex sets
                 self_set = set([tuple(vert) for vert in self_verts])
                 other_set = set([tuple(vert) for vert in other_verts_temp])
                 if self_set == other_set:
                     return True
-                #4. rotate the other graph by the rotation angle
+                # 4. rotate the other graph by the rotation angle
                 else:
                     if self.Cell == Square:
                         other_verts = rotate_normal(other_verts, 1)
                     elif self.Cell in (Hex, Tri):
-                        #not sure about this, will need further testing
-                        other_verts = rotate_normal(other_verts,2, axis=np.array([1,1,1]))
+                        # not sure about this, will need further testing
+                        other_verts = rotate_normal(
+                            other_verts, 2, axis=np.array([1, 1, 1]))
 
-        if self.Cell.dimensions==3:
+        if self.Cell.dimensions == 3:
             # will need to test this further
             other_min = min_coord(other_verts)
             other_verts = other_verts - other_min
@@ -216,34 +213,37 @@ class ConfigurationGraph:
         Cell = self.Cell
         in_vertices = np.array(in_vertices)
 
-        #1. check that the coordinates are of the correct shape
+        # 1. check that the coordinates are of the correct shape
         if in_vertices.shape[-1] != Cell.n_parameters:
-            raise ValueError(f"Incorrect input shape, expected tuples of length {Cell.n_parameters}")
-        
-        if len(in_vertices.shape) == 1: #prevent errors from entering 1D arrays
+            raise ValueError(
+                f"Incorrect input shape, expected tuples of length {Cell.n_parameters}")
+
+        if len(in_vertices.shape) == 1:  # prevent errors from entering 1D arrays
             in_vertices = np.array([in_vertices])
 
-        #2. check that the coordinate is valid in the graph lattice
+        # 2. check that the coordinate is valid in the graph lattice
         for vertex in in_vertices:
 
             if not(Cell.valid_coord(vertex)):
-                warnings.warn(f"{vertex} is not a valid coordinate, removing from graph")
+                warnings.warn(
+                    f"{vertex} is not a valid coordinate, removing from graph")
                 index = get_index(vertex, in_vertices)
                 in_vertices = np.delete(in_vertices, index, axis=0)
 
-        
-        in_vertices = np.unique(in_vertices, axis=0) #not sure a about axis here
-        
+        # not sure a about axis here
+        in_vertices = np.unique(in_vertices, axis=0)
+
         new_vertices = np.append(self.vertices, in_vertices, axis=0)
         new_graph = ConfigurationGraph(Cell, new_vertices, self.connect_type)
 
-        #3. if necessary, check connectivity of resulting array
-        if not(check_connectivity) or len(self.vertices)==0:
+        # 3. if necessary, check connectivity of resulting array
+        if not(check_connectivity) or len(self.vertices) == 0:
             return new_graph
         else:
-            if check_connectivity and not(len(self.vertices)==0):
+            if check_connectivity and not(len(self.vertices) == 0):
                 if not(self.is_connected()):
-                    warnings.warn(f'resulting graph of adding {in_vertices} would be disconnected, no vertices added')
+                    warnings.warn(
+                        f'resulting graph of adding {in_vertices} would be disconnected, no vertices added')
                     return self
                 else:
                     return new_graph
@@ -263,17 +263,16 @@ class ConfigurationGraph:
         2. If the graph is required to be connected, then the reduced graph must be connected.
         """
 
-
         Cell = self.Cell
         vertices = self.vertices
-        #1. check that the coordinates are of the correct shape
+        # check that the coordinates are of the correct shape
         if rm_vertices.shape[-1] != Cell.n_parameters:
-            raise ValueError(f"Incorrect input shape, expected tuples of length {Cell.n_parameters}")
-        
-        if len(rm_vertices.shape) == 1: #prevent errors from entering 1D arrays
+            raise ValueError(
+                f"Incorrect input shape, expected tuples of length {Cell.n_parameters}")
+
+        if len(rm_vertices.shape) == 1:  # prevent errors from entering 1D arrays
             rm_vertices = np.array([rm_vertices])
 
-        
         # edit the vertices array to remove the vertices
         for vertex in rm_vertices:
             try:
@@ -283,36 +282,35 @@ class ConfigurationGraph:
                 continue
         # generate new graph
         new_graph = ConfigurationGraph(Cell, vertices, self.connect_type)
-
-
-        #2. check connectivity of resulting array
+        # 2. check connectivity of resulting array
         if not check_connectivity:
             return new_graph
         else:
             if not(new_graph).is_connected():
-                warnings.warn(f"resulting graph of removing {rm_vertices} would be disconnected, no vertices removed")
+                warnings.warn(
+                    f"resulting graph of removing {rm_vertices} would be disconnected, no vertices removed")
                 return self
             else:
                 return new_graph
-        
 
     def __eq__(self, other) -> bool:
         """Check if two graphs are congruent (same edge graphs)
         Currently does not work for rotational symmetry
         """
         return self.isomorphic(other)
-    
+
     def __getitem__(self, index):
         return self.vertices[index]
 
-    def __contains__ (self, other: Iterable)->bool:
+    def __contains__(self, other: Iterable) -> bool:
         return other.tolist() in self.vertices.tolist()
 
 
-def equals(graph1: ConfigurationGraph, graph2: ConfigurationGraph)->bool:
+def equals(graph1: ConfigurationGraph, graph2: ConfigurationGraph) -> bool:
     """Check if two graphs are equal
     """
     return graph1 == graph2
+
 
 def connected(graph: ConfigurationGraph):
     """Checks if the graph is connected
@@ -320,49 +318,52 @@ def connected(graph: ConfigurationGraph):
     """
     return graph.is_connected()
 
-def get_index(vertex:np.array, vertices:np.array)->int:
-        """get the index of a vertex in a set of vertices
-        
-        parameters:
-        :param vertex: the vertex to find the index of
-        :param vertices: the set of vertices to search
-        :return: the index of the vertex in the set of vertices
-        """
-        # This solution could be improved to handle multiple vertices in a single call, using a vectorised method
-        try:
-            return int(np.where(np.all(vertices == vertex, axis=1))[0][0])
-        except IndexError:
-            return None
 
-def min_coord(vertices:np.array)->np.array:
+def get_index(vertex: np.array, vertices: np.array) -> int:
+    """get the index of a vertex in a set of vertices
+
+    parameters:
+    :param vertex: the vertex to find the index of
+    :param vertices: the set of vertices to search
+    :return: the index of the vertex in the set of vertices
+    """
+    # This solution could be improved to handle multiple vertices in a single call, using a vectorised method
+    try:
+        return int(np.where(np.all(vertices == vertex, axis=1))[0][0])
+    except IndexError:
+        return None
+
+
+def min_coord(vertices: np.array) -> np.array:
     """find the minimum coordinate in a set of vertices
-    
+
     parameters:
     :param vertices: the set of vertices to search
     :return: the index of the vertex in the set of vertices
     """
-    #we perform this iteratively to deal with the existence of multiple minimum coordinates 
+    # we perform this iteratively to deal with the existence of multiple minimum coordinates
     # depending on the rotation and shape
     # we can make use of the fact that to add a vector to the matrix, it must be unique
     for i in range(vertices.shape[-1]):
-        min_index = np.argmin(vertices[:,i])
+        min_index = np.argmin(vertices[:, i])
 
         if type(min_index) == np.int64:
 
             return vertices[min_index]
 
-def max_coord(vertices:np.array)->np.array:
+
+def max_coord(vertices: np.array) -> np.array:
     """find the maximum coordinate in a set of vertices
-    
+
     parameters:
     :param vertices: the set of vertices to search
     :return: the index of the vertex in the set of vertices
     """
-    #we perform this iteratively to deal with the existence of multiple minimum coordinates 
+    # we perform this iteratively to deal with the existence of multiple minimum coordinates
     # depending on the rotation and shape
     # we can make use of the fact that to add a vector to the matrix, it must be unique
     for i in range(vertices.shape[-1]):
-        max_index = np.argmax(vertices[:,i])
+        max_index = np.argmax(vertices[:, i])
 
         if type(max_index) == np.int64:
 
@@ -371,6 +372,7 @@ def max_coord(vertices:np.array)->np.array:
 
 def add_vertices(graph: ConfigurationGraph, in_vertices: np.array, check_connectivity=True) -> ConfigurationGraph:
     return graph.add_vertices(in_vertices, check_connectivity)
+
 
 def remove_vertices(graph: ConfigurationGraph, rm_vertices: np.array, check_connectivity=True) -> ConfigurationGraph:
     return graph.remove_vertices(rm_vertices, check_connectivity)

mrrvis/geometry_utils.py
@@ -8,17 +8,19 @@ from typing import Generator, Union
 import numpy as np
 
 
-def r_from_normal(angle, normal, discretise=True):
-    """rotation matrix from a unit vector normal to the plane of rotation in discrete space
+def r_from_normal(angle, normal, discrete=True):
+    """provides a rotation matrix from a unit vector normal to the plane of rotation
+
+    by default this is cast to int to avoid floating point errors
     Parameters:
     :param angle: float: angle to rotate by
     :param normal: np.ndarray: unit vector normal to the plane of rotation
+    :param discrete: bool: whether to discretise the rotation matrix
     :return: np.ndarray: rotation matrix
     """
-    try:
-        normal = np.array(normal)
-    except:
-        raise ValueError('normal must be sequence')
+    normal = np.array(normal)
+    if normal.shape == ():
+        raise ValueError('normal must be specified')
 
     x, y, z = normal
     c = np.cos(angle)
@@ -28,16 +30,32 @@ def r_from_normal(angle, normal, discretise=True):
         [y*x*(1-c)+z*s, y*y*(1-c)+c, y*z*(1-c)-x*s],
         [x*z*(1-c)-y*s, y*z*(1-c)+x*s, z*z*(1-c)+c]
     ])
-    if discretise:
+    if discrete:
         return r.astype(int)
     else:
         return r
 
 
+def norm_from_str(axis: str) -> np.ndarray:
+    """generate a normal vector from a string representing an axis"""
+
+    normal_dict = {
+        'x': np.array([1, 0, 0]),
+        'y': np.array([0, 1, 0]),
+        'z': np.array([0, 0, 1])
+    }
+    norm = np.array([0, 0, 0])
+    for c in ['x', 'y', 'z']:
+        norm += normal_dict[c] if c in axis else 0
+    if np.all(norm == 0):
+        raise ValueError(
+            'axis must either be a string representing the axis or a vector normal ')
+
+    return norm
+
 
 def rotate_normal(array: np.ndarray, turns: int, base_angle: float = np.pi/2, around: np.ndarray = None, axis: Union[str, np.array] = None, ) -> np.ndarray:
     """rotate an set of points counterclockwise around a point, by default the origin in discrete space
-    (negatives are clockwise)
 
     Parameters:
     :param array: np.ndarray: array of points to rotate
@@ -45,61 +63,40 @@ def rotate_normal(array: np.ndarray, turns: int, base_angle: float = np.pi/2, ar
     :param base_angle: float: angle to rotate by
     :param around: np.ndarray: point to rotate around
     :param axis: str: axis to rotate around or a vector normal to the plane of rotation
-
     :return: np.ndarray: rotated array
 
     for a description of the axis and angle rotation, see:
     https://en.wikipedia.org/wiki/Rotation_matrix#Axis_and_angle
     """
-
+    array = np.array(array)
+    dims = array.shape[-1]
+    theta = turns * base_angle
     num_rots = 2*np.pi/base_angle
-    # nothing about this function is inherently discrete, so this check might need to be removed should the library be expanded to real spaces
     if abs(num_rots-int(num_rots)) > 0.001:
         raise ValueError(
             f'the base angle should divide 2pi; 2pi/base_angle must be an integer')
     if type(turns) != int:
         raise ValueError('turns must be an integer')
 
-    dims = array.shape[-1]
-    theta = turns * base_angle
+    # set around to the origin if not specified
+    around = (np.array([0, 0]) if dims == 2 else np.array(
+        [0, 0, 0])) if around is None else np.array(around)
+
+    if around.shape[-1] != dims:
+        raise ValueError(
+            'around must be of equal dimension to array')
+
     if dims == 3:
-        if type(axis) == str:  # generate a normal vector from a string representing an axis
-            normal_dict = {
-                'x': np.array([1, 0, 0]),
-                'y': np.array([0, 1, 0]),
-                'z': np.array([0, 0, 1])
-            }
-            norm = np.array([0, 0, 0])
-            for c in ['x', 'y', 'z']:
-                if c in axis:
-
-                    norm += normal_dict[c]
-
-            
-        elif hasattr(axis, '__iter__'):  # axis is a vector: is already the normal
-            norm = np.array(axis)
-        else: # axis has to have a value in 3D as there is no such thing as a 'generic' 3D rotation, as rotations are inherently 2
-            raise ValueError('axis must be specified for 3D arrays')
-
-        if norm.tolist() not in [[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0], [1, 0, 1], [0, 1, 1], [1, 1, 1]]:
-                # hacky solution but cheap; if this method is extended to non-discrete spaces, this check should 
-                # be removed
-                raise ValueError('axis must either be a string representing the axis or a vector normal ')
-        
-        r = r_from_normal(theta, norm)
-
-    else: # 2D: a rotation in 2D is simply the first two components of a rotation on the plane z=0, i.e. normal [0,0,1]
+        norm = norm_from_str(axis) if type(axis) == str else np.array(axis)
+        norm = norm / np.linalg.norm(norm)
+        r = r_from_normal(theta, norm, discrete=False)
+    else:
         r = r_from_normal(theta, [0, 0, 1])[:2, :2]
 
-    if around is None:
-        around = np.array([0, 0]) if dims == 2 else np.array([0, 0, 0])
 
-    try:
-        return (r@(array-around).T).T + around
-    except ValueError as e:
-        raise ValueError(
-            'array and around must have the same trailing axes') from e
-
+    transformed_array = ((r@(array-around).T).T + around)
+    print(transformed_array)
+    return np.around(transformed_array).astype(np.int16)
 
 
 def isometric(array: np.ndarray) -> np.ndarray:
@@ -117,20 +114,51 @@ def isometric(array: np.ndarray) -> np.ndarray:
     vs = (xs+2*ys+zs)/np.sqrt(6)
     return np.array([us, vs]).T
 
-def cube_rotation_list(array: np.ndarray) -> Generator:
+
+def cube_rotation_list(coordinates: np.ndarray) -> Generator:
     """generator of 24 rotations in the regular octahedral rotation group S_4 for an array of 3D cartesian coordinates"""
     # based on https://stackoverflow.com/questions/16452383/how-to-get-all-24-rotations-of-a-3-dimensional-array
     # expanded for dealing with an array of coordinates
-    def roll(arr) : return rotate_normal(arr,1,axis = np.array([0,1,0]))
-    def turn(arr) : return rotate_normal(arr,1,axis =np.array([0,0,1])) # yaw
+    def roll(arr): return rotate_normal(
+        arr, 1, axis=np.array([0, 1, 0]))  # roll
+
+    def turn(arr): return rotate_normal(
+        arr, 1, axis=np.array([0, 0, 1]))  # yaw
+
     def sequence(arr):
-        for _side in range(2):
-            for _roll in range(3):
+        for _ in range(2):  # side
+            for _ in range(3):  # roll
                 arr = roll(arr)
                 yield arr
-                for _turn in range(3):
+                for _ in range(3):  # turn
                     arr = turn(arr)
                     yield arr
+            # switch 'side' so that the other 3 sides can be 'face up'
             arr = roll(turn(roll(arr)))
 
-    return sequence(array)
+    return sequence(coordinates)
+
+
+def square_rotation_list(array: np.ndarray) -> Generator:
+    """generator of the 4 rotations of a set of coordinates in a square lattice"""
+    yield array
+    for _ in range(3):
+        array = rotate_normal(array, 1)
+        yield array
+
+
+def tri_rotation_list(array: np.ndarray) -> Generator:
+    """generator of the 3 rotations of a set of coordinates in a triangular lattice"""
+    yield array
+    for _ in range(2):
+        array = rotate_normal(array, 1, base_angle=2*np.pi/3, axis='xyz')
+        yield array
+
+
+def hex_rotation_list(array: np.ndarray) -> Generator:
+    """generator of the 6 rotations of a set of coordinates in a hexagonal lattice"""
+    yield array
+    for _ in range(5):
+        array = rotate_normal(array, 1, base_angle=np.pi/3, axis=np.array([1,1,1]))
+
+        yield array

tests/test_geometry_utilities.py
@@ -1,7 +1,7 @@
 
 import pytest
 import numpy as np
-from mrrvis.geometry_utils import rotate_normal, isometric,r_from_normal, cube_rotation_list
+from mrrvis.geometry_utils import rotate_normal, isometric,r_from_normal, cube_rotation_list, square_rotation_list, tri_rotation_list, hex_rotation_list
 
 
 _3Dx_rotation_matrix = lambda theta: np.array([
@@ -53,7 +53,6 @@ def test_rot_around():
 
 
 
-
 def test_rot_single_item():
     arr = np.array([1,0])
     #because the function is defined for a matrix, it seems prudent to check that it works for a single array
@@ -61,10 +60,11 @@ def test_rot_single_item():
 
 def test_wrong_axis():
     arr = np.array([[1,0,0],[0,1,0],[0,0,1]])
-    with pytest.raises(ValueError):
-        rotate_normal(arr,1, axis='foo')
+    with pytest.raises(Exception): #ideally this should be a ValueError, but Pytest is behaving strangely, 
+        # it catches a value error but does not raise it, causing another error to happen due to None values
+        rotate_normal(arr,3, axis='foo')
 
-    with pytest.raises(ValueError):
+    with pytest.raises(Exception):
         rotate_normal(arr,1)
 
     #the axis should be irrelevant to 2D rotations
@@ -79,7 +79,7 @@ def test_wrong_turns():
 
 def test_rot_3D():
     arr = np.array([[5,0,0],[0,5,0],[0,0,5]])
-    print(rotate_normal(arr,1,axis='x'))
+
     assert np.all(rotate_normal(arr,1,axis='x') == np.array([[5,0,0],[0,0,5],[0,-5,0]]))
     assert np.all(rotate_normal(arr,1,axis='y') == np.array([[0,0,-5],[0,5,0],[5,0,0]]))
     assert np.all(rotate_normal(arr,1,axis='z') == np.array([[0,5,0],[-5,0,0],[0,0,5]]))
@@ -92,6 +92,12 @@ def test_rot_3D_composition():
     assert np.all(rotate_normal(arr,4, axis='xyz')==arr)
     assert np.all(rotate_normal(arr,4*5000, axis='xyz') == arr)
     assert np.all(rotate_normal(arr,2, axis='xy') == rotate_normal(arr,-2, axis='xy'))
+
+def test_rot_hex():
+    arr = np.array([[1,-1,0]])
+    assert np.all(rotate_normal(arr,1,axis='xyz',base_angle=np.pi/3) == rotate_normal(arr,1,axis='xzy',base_angle=np.pi/3))
+    print(r_from_normal(np.pi/3, np.array([1,1,1]),False))
+    assert np.all(rotate_normal(arr,1,axis=np.array([1,1,1]),base_angle=np.pi/3) == np.array([1,0,-1])) 
 
     
 # def test_mirror()
@@ -128,4 +134,23 @@ def test_cube_rot_generator():
 
     arr = [arr for arr in cube_rotation_list(np.array([[1,0,0],[0,1,0],[0,0,1]]))]
     assert len(arr) == 24
+    
+def test_square_rot_generator():
+    base_arr = np.array([[1,0],[0,1],[0,0]])
+    lst = square_rotation_list(base_arr)
+    check = base_arr
+    for shape in lst:
+        assert np.all(shape == check)
+        check = rotate_normal(check,1)
+
+def test_hex_rot_generator():
+    base_arr = np.array([[1,0,-1],[0,-1,1]])
+    lst = hex_rotation_list(base_arr)
+
+    check = base_arr
+    for shape in lst:
+        assert np.all(shape == check)
+        check = rotate_normal(check,1, axis=[1,1,1], base_angle=np.pi/3)
+        
+        
     